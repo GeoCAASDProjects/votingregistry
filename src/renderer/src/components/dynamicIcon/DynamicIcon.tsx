a34fa4ce54678dc768c1aabcdefa2f141a29d67c
Antonio-A-D-C <anton.y01@hotmail.com>
2024-06-20 09:11:38 -0400

    Save current data

---
+++ src/renderer/src/components/dynamicIcon/DynamicIcon.tsx
@@ -0,0 +1,20 @@
+// DynamicIcon.js
+import React from 'react';
+import * as Icons from '@mui/icons-material';
+
+interface IconI{
+iconName: string,
+ 
+}
+function DynamicIcon({ iconName, ...props }:IconI): JSX.Element|null {
+  const IconComponent = Icons[iconName];
+  console.log(IconComponent)
+  if (!IconComponent) {
+    console.error(`Icon "${iconName}" not found`);
+    return null; // You can return a default icon or an empty element here
+  }
+
+  return <IconComponent {...props} />;
+};
+
+export default DynamicIcon;
