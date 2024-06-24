59de981d267fcb6cb594fa8af5efaf2f26f0b502
Antonio-A-D-C <anton.y01@hotmail.com>
2024-06-24 15:34:16 -0400

    Save data, added create enclosures

---
+++ src/renderer/src/components/enclosureForm/EnclosureForm.tsx
@@ -0,0 +1,72 @@
+import Button from '../button/Button'
+
+import * as Yup from 'yup';
+import classes from './enclosureform.module.css'
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+export default function EnclosureCreateForm({submitData, defaultValues, isLoading}) {
+
+    const EnclosureSchema = Yup.object().shape({
+       name: Yup.string().required('Requerido'),
+       address: Yup.string().required('Requerido'),
+       longitude: Yup.number().required('Requerido'),
+       latitude: Yup.number().required('Requerido'),
+    });
+
+    
+    const initialValues = {
+        name: '',
+        address: '',
+        longitude:defaultValues.lng ?? 0,
+        latitude: defaultValues.lat ?? 0
+    }
+
+    return (
+
+        <Formik
+        initialValues={initialValues}
+        validationSchema={EnclosureSchema}
+        onSubmit={submitData}
+    >
+        {({ isSubmitting }) => (
+            <Form>
+                 <div className={classes['enclosure-form']}> 
+                <h3>Nuevo Recinto</h3>
+                <div style={{margin: "10px 0"}}>
+
+                <div  className={classes['input']}>
+                <label>Nombre</label>
+                <Field type="name" name="name" placeholder="Nombre"/>
+                <span style={{ color: "red" }}> <ErrorMessage name="name" component="div" /></span>
+                </div>
+                <div  className={classes['input']}>
+                <label>Dirección</label>
+                <Field id="address" name="address" placeholder="Dirección" />
+                <span style={{ color: "red" }}> <ErrorMessage name="address" component="div" /></span>
+                </div>
+                <div  className={classes['input']}>
+                <label>Longitud</label>
+                <Field type="number" id="longitude" name="longitude" placeholder="Longitud" />
+                <span style={{ color: "red" }}> <ErrorMessage name="longitude" component="div" /></span>
+                </div>
+                <div  className={classes['input']}>
+                <label>Latitud</label>
+                <Field  type="number" id="latitude" name="latitude" placeholder="Latitud" />
+                <span style={{ color: "red" }}> <ErrorMessage name="latitude" component="div" /></span>
+                </div>
+                </div>
+                
+                
+                <Button type="submit" title="Enviar" iconName="Send" isLoading={isLoading} center/>
+                  
+               
+        </div>
+
+            </Form>
+
+        )}
+    </Formik> 
+       
+   
+
+    )
+}
---
+++ src/renderer/src/components/enclosureInfo/EnclosureInfo.tsx
@@ -0,0 +1,109 @@
+import { CircularProgress } from "@mui/material"
+import Button from "../button/Button"
+import { Close } from "@mui/icons-material"
+import classes from './enclosureInfo.module.css'
+
+export default function EnclosureInfo({singleEnclosurePending, currentEnclosure, clearEnclosure, selectLocation}){
+    return (
+        <>
+        {singleEnclosurePending &&
+            <div style={{ display: "flex", width: "100%", alignContent: "center", alignItems: "center", justifyContent: "center" }}>
+                <CircularProgress color="inherit" size={30} />
+            </div>
+        }
+
+        {(!singleEnclosurePending && !!currentEnclosure) ?
+
+            <div>
+                <div style={{ width: "100%", display: "flex", justifyContent: "flex-end" }}>
+                    <Close onClick={clearEnclosure} />
+                </div>
+
+                <table className={classes['table']}>
+                    <thead>
+
+
+                    </thead>
+                    <tbody>
+                        <tr>
+
+                            <td>   <span style={{ fontWeight: "bold" }}>Nombre</span></td>
+                            <td>{currentEnclosure.name}</td>
+
+                        </tr>
+                        <tr>
+                            <td>   <span style={{ fontWeight: "bold" }}>Dirección</span></td>
+                            <td>{currentEnclosure.address}</td>
+
+                        </tr>
+                        <tr>
+                            <td>   <span style={{ fontWeight: "bold" }}>Coordenadas</span></td>
+                            <td>{`${currentEnclosure.longitude}, ${currentEnclosure.latitude}`}</td>
+
+                        </tr>
+
+                        <tr>
+                            <td>   <span style={{ fontWeight: "bold" }}>Colegios</span></td>
+                            <td>{currentEnclosure.schools.length ?? 0}</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+
+                <Button title="Editar" iconName="Edit" style={{ width: "100%", background: "#22224F", color: "#FFFFFF", margin: "5px 0px" }} />
+                <Button title="Descargar" iconName="Download" style={{ width: "100%", background: "#22224F", color: "#FFFFFF", margin: "5px 0px" }} />
+                <Button title="Borrar" iconName="Delete" style={{ background: "#22224F", width: "100%", color: "#FFFFFF", margin: "5px 0px" }} />
+
+                <h3>Colegios</h3>
+
+                {currentEnclosure?.schools.length > 0 ?
+                    <div>
+
+                        <table className={classes['table']}>
+                            <thead>
+                                <th>   <span style={{ fontWeight: "bold" }}>Nombre</span></th>
+                                <th>   <span style={{ fontWeight: "bold" }}>Personas</span></th>
+                            </thead>
+                            <tbody>
+                                {currentEnclosure?.schools.length > 0 && currentEnclosure.schools.map((enclosure) => (
+                                    <tr>
+
+
+                                        <td>{enclosure.name}</td>
+                                        <td>{enclosure.members.length ?? 0}</td>
+
+                                    </tr>
+                                ))}
+
+                            </tbody>
+                        </table>
+                        <Button title="Descargar" iconName="Download" style={{ width: "100%", background: "#22224F", color: "#FFFFFF", margin: "5px 0px" }} />
+                    </div> :
+                    <div style={{ margin: "5px 0px" }}>
+                        <p>Este recinto no cuento con colegios actualmente</p>
+                    </div>
+
+                }
+                <Button title="Añadir colegio" iconName="Add" style={{ width: "100%", background: "#22224F", color: "#FFFFFF", margin: "5px 0px" }} />
+                <Button title="Subir Colegios" iconName="Upload" style={{ width: "100%", background: "#22224F", color: "#FFFFFF", margin: "5px 0px" }} />
+            </div>
+            :
+
+            <>
+                <Button title="Añadir recintos" iconName="Add" onClick={selectLocation} style={{ width: "100%", background: "#22224F", color: "#FFFFFF", margin: "5px 0px" }} />
+                <Button title="Añadir sector" iconName="Polyline" style={{ width: "100%", background: "#22224F", color: "#FFFFFF", margin: "5px 0px" }} />
+                <Button title="Subir Archivos" iconName="Upload" style={{ width: "100%", background: "#22224F", color: "#FFFFFF", margin: "5px 0px" }} />
+
+
+
+
+
+            </>
+
+
+
+        }     
+        </>
+    )
+}
